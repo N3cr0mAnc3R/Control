58699fce0aae63e218fd99fc9fe890851f48d545
Nikolai Shiriaev <theShirNick@gmail.com>
2019-04-23 15:40:53 +0300

    Миграция bootstrap 3 -> 4

---
+++ WebApp/Controllers/ApplicationController.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Http;
+using System.Web.Mvc;
+
+namespace WebApp.Controllers
+{
+    public class ApplicationController : Controller
+    {
+        public ActionResult GetApplication()
+        {
+           
+            return View();
+         
+        }
+    }
+}
