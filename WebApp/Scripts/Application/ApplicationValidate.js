0038f432ffdab26025cc7f948a8eeeb9b6a4b111
Nikolai Shiriaev <theShirNick@gmail.com>
2019-04-24 12:22:10 +0300

    vue косячит

---
+++ WebApp/Scripts/Application/ApplicationValidate.js
@@ -0,0 +1,16 @@
+﻿Window.onload = function () {
+    const app = new Vue({
+        el: "#form",
+        data: {
+            Text: "11111",
+            Files: [],
+            Errors: []
+        },
+        methods: {
+            InputFileValidate: function () {
+                console.log(event.target.files);
+            }
+        }
+
+    })
+}
