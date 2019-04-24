5962cfd5631544fb6b0a01cbdb322f6054ce3b45
Nikolai Shiriaev <theShirNick@gmail.com>
2019-04-24 11:40:04 +0300

    прикрепил vue

--- WebApp/App_Start/BundleConfig.cs
+++ WebApp/App_Start/BundleConfig.cs
@@ -22,6 +22,8 @@ namespace WebApp
             bundles.Add(new ScriptBundle("~/bundles/bootstrap").Include(
                       "~/Scripts/bootstrap.js"));
 
+            bundles.Add(new ScriptBundle("~/bundles/vue").Include(
+                      "~/Scripts/vue.js"));
             bundles.Add(new StyleBundle("~/Content/css").Include(
                       "~/Content/bootstrap.css",
                       "~/Content/site.css"));
