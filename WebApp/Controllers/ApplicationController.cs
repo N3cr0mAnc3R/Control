f830dba8ebe9aa0ac5eca3e7ca96bd1438415a67
Nikolai Shiriaev <theShirNick@gmail.com>
2019-04-24 11:26:15 +0300

    Добавлена корявая форма для заявки + модель и контроллер к ней

--- WebApp/Controllers/ApplicationController.cs
+++ WebApp/Controllers/ApplicationController.cs
@@ -1,9 +1,11 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Text;
 using System.Web;
 using System.Web.Http;
 using System.Web.Mvc;
+using WebApp.Models;
 
 namespace WebApp.Controllers
 {
@@ -15,5 +17,30 @@ namespace WebApp.Controllers
             return View();
          
         }
+
+
+        public ActionResult SubmitApplication(ApplicationModel model)
+        {
+            if (ModelState.IsValid)
+            {
+                StringBuilder fileNames = new StringBuilder();
+                fileNames.Append("<ul>");
+                foreach (HttpPostedFileBase file in model.Files)
+                {
+                    //You can do something with the files here like save them to disk
+                    fileNames.Append("<li>");
+                    fileNames.Append(file.FileName);
+                    fileNames.Append("</li>");
+                }
+                fileNames.Append("</ul>");
+                TempData["FileNames"] = fileNames.ToString();
+                 return View();
+            }
+
+            return View();
+
+        }
+
+
     }
 }
---
+++ WebApp/Models/ApplicationModel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace WebApp.Models
+{
+    public class ApplicationModel
+    {
+        [Required]
+        public string Text { get; set; }
+        public HttpPostedFileBase[] Files { get; set; }
+    }
+}
