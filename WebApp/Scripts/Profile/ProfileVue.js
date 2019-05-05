e17cc1e1ea0d24d2a2e05b012267e35b8589482d
Павел <paul-profi@mail.ru>
2019-05-05 11:48:55 +0300

    Чуть изменены комментарии

--- WebApp/Scripts/Profile/ProfileVue.js
+++ WebApp/Scripts/Profile/ProfileVue.js
@@ -1,81 +1,77 @@
 ﻿window.onload = function () {
-	const app = new Vue({
-		el: "#applications",
-		data: {
-			applications: [],
-			comment:''
+    const app = new Vue({
+        el: "#applications",
+        data: {
+            applications: [],
+            comment: ''
 
-		},
-		methods: {
-			toggleComments: function (Id) {
-				let appl = this.applications.find(a => a.Id === Id);//обращение из заполненного заранее массива обращений...
-				Vue.nextTick(function () {
-					appl.IsOpened = !appl.IsOpened;
-					if (appl.IsOpened) {
-						app.SelectCommentsByApplicationId(appl.Id);
-						//appl.comments =app.SelectCommentsByApplicationId(appl.Id);//заполнение комметариев для данного обращения
-						
-					}
-				});
-			},
-			changeComment: function (event) {
-				this.comment = event.target.value;
-			},
-			addComment: function (applicationId) {
-				$.ajax({
-					url: "/profile/AddComment",
-					type: "POST",
-					async: false,
-					data: { ApplicationId: applicationId, Text:app.comment}
-					}
-				);
-			},
-			selectApplicationsByUserId: function () {
-				$.ajax({
-					url: "/profile/SelectApplicationsByUserId",
-					type: "POST",
-					async: false,
-					success: function (applications) {
-						Vue.nextTick(function () {
-							
-							console.log(applications);
-							applications.forEach(function (application) {
-								application.IsOpened = false;
-								app.applications.push(application);
-							});
-						});
+        },
+        methods: {
+            toggleComments: function (Id) {
+                let appl = this.applications.find(a => a.Id === Id);//обращение из заполненного заранее массива обращений...
+                Vue.nextTick(function () {
+                    app.SelectCommentsByApplicationId(appl);
+                    //appl.comments =app.SelectCommentsByApplicationId(appl.Id);//заполнение комметариев для данного обращения
 
-					}
-				});
-			},
-			SelectCommentsByApplicationId: function (ApplicationId) {
-			
-				$.ajax({
-					url: "/profile/SelectCommentsByApplicationId",
-					type: "POST",
-					data: { ApplicationId: ApplicationId },
-					async: false,
-					success: function (comments) {
-						let appl = app.applications.find(a => a.Id === ApplicationId);//обращение из заполненного заранее массива 
-						Vue.nextTick(function () {		
-							let applicationComments = [];
-							comments.forEach(function (comment) {
-								applicationComments.push(comment);
-							});
-							appl.comments = applicationComments;
-						
-						});
+                });
+            },
+            changeComment: function (event) {
+                this.comment = event.target.value;
+            },
+            addComment: function (applicationId) {
+                $.ajax({
+                    url: "/profile/AddComment",
+                    type: "POST",
+                    async: false,
+                    data: { ApplicationId: applicationId, Text: app.comment }
+                }
+                );
+            },
+            selectApplicationsByUserId: function () {
+                $.ajax({
+                    url: "/profile/SelectApplicationsByUserId",
+                    type: "POST",
+                    async: false,
+                    success: function (applications) {
+                        Vue.nextTick(function () {
 
-					}
-				});
-			}
+                            console.log(applications);
+                            applications.forEach(function (application) {
+                                application.IsOpened = false;
+                                app.applications.push(application);
+                            });
+                        });
+
+                    }
+                });
+            },
+            SelectCommentsByApplicationId: function (application) {
+                if (!application.IsOpened)
+                $.ajax({
+                    url: "/profile/SelectCommentsByApplicationId",
+                    type: "POST",
+                    data: { ApplicationId: application.Id },
+                    async: false,
+                    success: function (comments) {
+                        Vue.nextTick(function () {
+                            let applicationComments = [];
+                            comments.forEach(function (comment) {
+                                applicationComments.push(comment);
+                            });
+                            appl.comments = applicationComments;
+                            application.IsOpened = !application.IsOpened;
+                        });
+
+                    }
+                });
+            }
 
 
-		},
-		beforeMount() {
-			this.selectApplicationsByUserId();
-		}
-		
+        },
+        beforeMount() {
+            this.selectApplicationsByUserId();
+        }
 
-	});
+
+    });
 };
