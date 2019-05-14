b1ef14ab9f1bb2f308429a455b37e44d918f6323
Павел <paul-profi@mail.ru>
2019-05-14 14:18:58 +0300

    Избавились от nextTick-а с помощью правильных средств

--- WebApp/Scripts/Profile/ProfileVue.js
+++ WebApp/Scripts/Profile/ProfileVue.js
@@ -1,120 +1,116 @@
 ﻿window.onload = function () {
-	const app = new Vue({
-		el: "#applications",
-		data: {
-			applications: [],
-			comment: '',
-			img: ''
+    const app = new Vue({
+        el: "#applications",
+        data: {
+            applications: [],
+            comment: '',
+            img: '',
+            objForLoading: {
+                loading: false,
+                loaded: true
+            }
 
-		},
-		methods: {
-			toggleComments: function (Id) {
-				let appl = this.applications.find(a => a.Id === Id);//обращение из заполненного заранее массива обращений...
-				Vue.nextTick(function () {
-					app.SelectCommentsByApplicationId(appl);
-					//appl.comments =app.SelectCommentsByApplicationId(appl.Id);//заполнение комметариев для данного обращения
+        },
+        methods: {
+            toggleComments: function (Id) {
+                let appl = this.applications.find(a => a.Id === Id);//обращение из заполненного заранее массива обращений...
+                    app.SelectCommentsByApplicationId(appl);
+                    //appl.comments =app.SelectCommentsByApplicationId(appl.Id);//заполнение комметариев для данного обращения
 
-				});
-			},
-			changeComment: function (event) {//байндинг комментария с vue 
-				this.comment = event.target.value;
-			},
-			addComment: function (applicationId) {
-				$.ajax({
-					url: "/profile/AddComment",
-					type: "POST",
-					async: false,
-					data: { ApplicationId: applicationId, Text: app.comment },
-					success: function () {
-						let appl = app.applications.find(a => a.Id === applicationId);
-						appl.IsOpened = false;//немного костыля  
-						app.SelectCommentsByApplicationId(appl);
-					}
-				}
+            },
+            changeComment: function (event) {//байндинг комментария с vue 
+                this.comment = event.target.value;
+            },
+            addComment: function (applicationId) {
+                $.ajax({
+                    url: "/profile/AddComment",
+                    type: "POST",
+                    async: false,
+                    data: { ApplicationId: applicationId, Text: app.comment },
+                    success: function () {
+                        let appl = app.applications.find(a => a.Id === applicationId);
+                        appl.IsOpened = false;//немного костыля  
+                        app.SelectCommentsByApplicationId(appl);
+                    }
+                }
 
-				);
-
-
-
-
-			},
-			selectApplicationsByUserId: function () {
-				$.ajax({
-					url: "/profile/SelectApplicationsByUserId",
-					type: "POST",
-					async: false,
-					success: function (applications) {
-						Vue.nextTick(function () {
-
-							console.log(applications);
-							applications.forEach(function (application) {
-								app.GetApplicationImages(application);
-								application.IsOpened = false;
-								app.applications.push(application);
-								console.log(application);
-							});
-						});
-
-					}
-				});
-			},
-			GetApplicationImages: function (application) {
-					$.ajax({
-						url: "/application/GetApplicationImages",
-						type: "POST",
-						data: { Id: application.Id },
-						async: false,
-						success: function (imgs) {
-							Vue.nextTick(function () {
-								let applicationImgs = [];
-								imgs.forEach(function (img) {
-									applicationImgs.push('data:image/png;base64, ' + img);
-								});
-								application.imgs = applicationImgs;
-								
-							});
-
-						}
-					});
-			},
-
-			SelectCommentsByApplicationId: function (application, offset = 1) {
-				if (!application.IsOpened) {
-					$.ajax({
-						url: "/profile/SelectCommentsByApplicationId",
-						type: "POST",
-						data: { ApplicationId: application.Id, Offset: offset },
-						async: false,
-						success: function (obj) {
-							Vue.nextTick(function () {
-								let applicationComments = [];
-								obj.Comments.forEach(function (comment) {
-									applicationComments.push(comment);
-								});
-								application.comments = applicationComments;
-								application.IsOpened = !application.IsOpened;
-								console.log(obj.CommentNumber);
-
-								application.commentPagesNumber = parseInt(obj.CommentNumber / 10);
-								console.log("SelectCommentsByApplicationId");
-							});
-
-						}
-					});
-				}
-				else {
-					application.IsOpened = !application.IsOpened;
-				}
-			}
+                );
 
 
-		},
-
-		beforeMount() {
-			this.selectApplicationsByUserId();
-		
-		}
 
 
-	});
+            },
+            selectApplicationsByUserId: function () {
+                var self = this;
+                $.ajax({
+                    url: "/profile/SelectApplicationsByUserId",
+                    type: "POST",
+                    async: false,
+                    success: function (applications) {
+                            applications.forEach(function (application) {
+                                self.GetApplicationImages(application);
+                                application.IsOpened = false;
+                                self.applications.push(application);
+                                console.log(application);
+                            });
+                        self.objForLoading.loading = false;
+                        self.objForLoading.loaded = true;
+                    }
+                });
+            },
+            GetApplicationImages: function (application) {
+                var self = this;
+                $.ajax({
+                    url: "/application/GetApplicationImages",
+                    type: "POST",
+                    data: { Id: application.Id },
+                    async: false,
+                    success: function (imgs) {
+                        let applicationImgs = [];
+                        imgs.forEach(function (img) {
+                            applicationImgs.push('data:image/png;base64, ' + img);
+                        });
+                        self.$set(application, 'imgs', applicationImgs);
+                    }
+                });
+            },
+
+            SelectCommentsByApplicationId: function (application, offset = 1) {
+                if (!application.IsOpened) {
+                    $.ajax({
+                        url: "/profile/SelectCommentsByApplicationId",
+                        type: "POST",
+                        data: { ApplicationId: application.Id, Offset: offset },
+                        async: false,
+                        success: function (obj) {
+                            let applicationComments = [];
+                            obj.Comments.forEach(function (comment) {
+                                applicationComments.push(comment);
+                            });
+                            app.$set(application, 'comments', applicationComments);
+                            application.IsOpened = !application.IsOpened;
+                            console.log(obj.CommentNumber);
+
+                            app.$set(application, 'commentPagesNumber', parseInt(obj.CommentNumber / 10));
+                            console.log("SelectCommentsByApplicationId");
+                        }
+                    });
+                }
+                else {
+                    application.IsOpened = !application.IsOpened;
+                }
+            }
+
+
+        },
+
+        beforeMount() {
+            this.objForLoading.loading = true;
+            this.objForLoading.loaded = false;
+            this.selectApplicationsByUserId();
+
+        }
+
+
+    });
 };
