f7d7ec2e3e653eba041a5f51899806e29b95dfc3
Nikolai Shiriaev <theShirNick@gmail.com>
2019-05-20 13:16:54 +0300

    Google OAuth2

--- WebApp/App_Start/Startup.Auth.cs
+++ WebApp/App_Start/Startup.Auth.cs
@@ -64,11 +64,11 @@ namespace WebApp
                appId: "297392017804204",
                appSecret: "2b46856256c4c540b9f3d1149e9507db");
 
-            //app.UseGoogleAuthentication(new GoogleOAuth2AuthenticationOptions()
-            //{
-            //    ClientId = "662359730750-3395ini7kh2imkvf4tni5shvcdi634nr.apps.googleusercontent.com",
-            //    ClientSecret = "YzJwaoa6GnGiBN_4uya2cu2B"
-            //});
+            app.UseGoogleAuthentication(new GoogleOAuth2AuthenticationOptions()
+            {
+                ClientId = "662359730750-3395ini7kh2imkvf4tni5shvcdi634nr.apps.googleusercontent.com",
+                ClientSecret = "YzJwaoa6GnGiBN_4uya2cu2B"
+            });
         }
     }
 }
