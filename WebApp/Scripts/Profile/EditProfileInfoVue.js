84db435015f6a4a2e9d5e9473e73d1a158f92b3f
N3cr0mAnc3R <paul-profi@mail.ru>
2019-05-22 23:01:11 +0300

    Исправлена ошибка illegal invocation

--- WebApp/Scripts/Profile/EditProfileInfoVue.js
+++ WebApp/Scripts/Profile/EditProfileInfoVue.js
@@ -1,57 +1,56 @@
 ﻿window.onload = function () {
-	const app = new Vue({
-		el: "#app",
-		data: {
+    const app = new Vue({
+        el: "#app",
+        data: {
 
-			img: '',
-			Files: [],
-			objForLoading: {
-				loading: false,
-				loaded: true
-			}
+            img: '',
+            Files: [],
+            objForLoading: {
+                loading: false,
+                loaded: true
+            }
 
-		},
-		methods: {
-			InputFileValidate: function () {
-				var x = event.target.files;
-				var re = /(?:\.([^.]+))?$/;
+        },
+        methods: {
+            InputFileValidate: function () {
+                var x = event.target.files;
+                var re = /(?:\.([^.]+))?$/;
 
-				for (i = 0; i < x.length; i++) {
-					x[i].Extension = re.exec(x[i].name)[1];
-					x[i].UploadDate = new Date();
-					x[i].Name = x[i].name.substr(0, x[i].name.lastIndexOf('.'));
-					app.Files[i] = x[i];
-				}
-			},
-			saveProfileChanges: function () {
+                for (i = 0; i < x.length; i++) {
+                    x[i].Extension = re.exec(x[i].name)[1];
+                    x[i].UploadDate = new Date();
+                    x[i].Name = x[i].name.substr(0, x[i].name.lastIndexOf('.'));
+                    app.Files[i] = x[i];
+                }
+            },
+            saveProfileChanges: function () {
 
-				var ajaxData = new FormData();
-console.log($("#Files")[0].files);
-				ajaxData.append('Files', $("#Files")[0].files[0]);
-				
-				$.ajax({
-					url: "/profile/FileUpload",
-					type: "POST",
-					async: false,
-					data:  ajaxData ,
-					success: function () {
+                var ajaxData = new FormData();
+                console.log($("#Files")[0].files[0]);
+                ajaxData.append('Files', $("#Files")[0].files[0]);
 
-					}
-				}
+                $.ajax({
+                    url: "/profile/FileUpload",
+                    type: "POST",
+                    async: false,
+                    contentType: false,
+                    processData: false,
+                    data: ajaxData,
+                    success: function () {
+                    }
+                });
 
-				);
+            }
+        },
 
-			}
-		},
-
-		beforeMount() {
-			this.objForLoading.loading = true;
-			this.objForLoading.loaded = false;
-
-
-		}
+        beforeMount() {
+            this.objForLoading.loading = true;
+            this.objForLoading.loaded = false;
 
 
-	
-	});
+        }
+
+
+
+    });
 };
